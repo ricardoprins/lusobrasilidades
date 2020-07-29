e17fb5f32b8daa0042e918ed4974829df1845f97
Ricardo Prins <ricardo@iamprins.com>
2020-07-28 23:21:30 -0600

    unaccent?

--- db/crud.py
+++ db/crud.py
@@ -1,23 +1,27 @@
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.functions import ReturnTypeFromArgs
 from . import models
 
+class unaccent(ReturnTypeFromArgs):
+    pass
+
 def get_local_portugal(db: Session):
-    return db.query(models.Portugal)
+    return db.query(unaccent(models.Portugal))
 
 def get_portugal_distrito(db: Session, distrito: str):
-    return db.query(models.Portugal).filter(models.Portugal.distrito.ilike(distrito))
+    return db.query(models.Portugal).filter(unaccent(models.Portugal.distrito).ilike(distrito))
 
 def get_portugal_municipio(db: Session, municipio: str):
-    return db.query(models.Portugal).filter(models.Portugal.municipio.ilike(municipio))
+    return db.query(models.Portugal).filter(unaccent(models.Portugal.municipio).ilike(municipio))
 
 def get_portugal_freguesia(db: Session, freguesia: str):
-    return db.query(models.Portugal).filter(models.Portugal.freguesia.ilike(freguesia))
+    return db.query(models.Portugal).filter(unaccent(models.Portugal.freguesia).ilike(freguesia))
 
 def get_local_brasil(db: Session):
     return db.query(models.Brasil)
 
 def get_brasil_estado(db: Session, estado: str):
-    return db.query(models.Brasil).filter(models.Brasil.estado.ilike(estado))
+    return db.query(models.Brasil).filter(unaccent(models.Brasil.estado).ilike(estado))
 
 def get_brasil_municipio(db: Session, municipio: str):
-    return db.query(models.Brasil).filter(models.Brasil.municipio.ilike(municipio))
+    return db.query(models.Brasil).filter(unaccent(models.Brasil.municipio).ilike(municipio))
