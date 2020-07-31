9426512bda816ac30a338a88a766cd89cc3ac4b3
Ricardo Prins <ricardo@iamprins.com>
2020-07-31 10:02:00 -0600

    fixing heroku database mess

--- db/database.py
+++ db/database.py
@@ -8,7 +8,7 @@ DB Configuration
 File, engine, session and base
 """
 
-DATABASE_URL = 'postgres://lmkbmpjehkurvv:4662b55666124d08087aa6547c3b1597dcd845c995cd6a65fdb5ecb3ee96b453@ec2-3-215-83-17.compute-1.amazonaws.com:5432/d1g9c93mj67gr3'
+# DATABASE_URL = 'postgres://lmkbmpjehkurvv:4662b55666124d08087aa6547c3b1597dcd845c995cd6a65fdb5ecb3ee96b453@ec2-3-215-83-17.compute-1.amazonaws.com:5432/d1g9c93mj67gr3'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
