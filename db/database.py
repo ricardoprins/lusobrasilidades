988cb727c4f7068595b7b0de52fd2e1ee8b976dd
Ricardo Prins <ricardo@iamprins.com>
2020-08-11 21:58:55 -0600

    fixing import

--- db/database.py
+++ db/database.py
@@ -1,5 +1,5 @@
 import os
-from . import config
+# from . import config
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
